d329fb5f1353b9d6e4d48f3363530babe9447281
eronildo.fsilva <eronildo.fsilva@ITA0557226W10-1>
2024-08-06 19:57:11 -0300

    Adicionar arquivos de projeto.

---
+++ Jogo da Velha/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Jogo_da_Velha
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn_botão1 = new Button();
+            btn_botão2 = new Button();
+            btn_botão3 = new Button();
+            btn_botão4 = new Button();
+            btn_botão5 = new Button();
+            btn_botão6 = new Button();
+            btn_botão7 = new Button();
+            btn_botão8 = new Button();
+            btn_botão9 = new Button();
+            label1 = new Label();
+            btn_x = new Button();
+            btn_bolinha = new Button();
+            btn_iniciar = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            btn_inicio = new Button();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // btn_botão1
+            // 
+            btn_botão1.Location = new Point(78, 129);
+            btn_botão1.Name = "btn_botão1";
+            btn_botão1.Size = new Size(118, 135);
+            btn_botão1.TabIndex = 0;
+            btn_botão1.UseVisualStyleBackColor = true;
+            // 
+            // btn_botão2
+            // 
+            btn_botão2.Location = new Point(202, 129);
+            btn_botão2.Name = "btn_botão2";
+            btn_botão2.Size = new Size(105, 135);
+            btn_botão2.TabIndex = 1;
+            btn_botão2.UseVisualStyleBackColor = true;
+            // 
+            // btn_botão3
+            // 
+            btn_botão3.Location = new Point(313, 129);
+            btn_botão3.Name = "btn_botão3";
+            btn_botão3.Size = new Size(101, 135);
+            btn_botão3.TabIndex = 2;
+            btn_botão3.UseVisualStyleBackColor = true;
+            // 
+            // btn_botão4
+            // 
+            btn_botão4.Location = new Point(78, 270);
+            btn_botão4.Name = "btn_botão4";
+            btn_botão4.Size = new Size(118, 132);
+            btn_botão4.TabIndex = 3;
+            btn_botão4.UseVisualStyleBackColor = true;
+            // 
+            // btn_botão5
+            // 
+            btn_botão5.Location = new Point(202, 270);
+            btn_botão5.Name = "btn_botão5";
+            btn_botão5.Size = new Size(105, 132);
+            btn_botão5.TabIndex = 4;
+            btn_botão5.UseVisualStyleBackColor = true;
+            // 
+            // btn_botão6
+            // 
+            btn_botão6.Location = new Point(313, 270);
+            btn_botão6.Name = "btn_botão6";
+            btn_botão6.Size = new Size(101, 132);
+            btn_botão6.TabIndex = 5;
+            btn_botão6.UseVisualStyleBackColor = true;
+            // 
+            // btn_botão7
+            // 
+            btn_botão7.Location = new Point(78, 408);
+            btn_botão7.Name = "btn_botão7";
+            btn_botão7.Size = new Size(118, 130);
+            btn_botão7.TabIndex = 6;
+            btn_botão7.UseVisualStyleBackColor = true;
+            // 
+            // btn_botão8
+            // 
+            btn_botão8.Location = new Point(202, 408);
+            btn_botão8.Name = "btn_botão8";
+            btn_botão8.Size = new Size(105, 130);
+            btn_botão8.TabIndex = 7;
+            btn_botão8.UseVisualStyleBackColor = true;
+            // 
+            // btn_botão9
+            // 
+            btn_botão9.Location = new Point(313, 408);
+            btn_botão9.Name = "btn_botão9";
+            btn_botão9.Size = new Size(101, 130);
+            btn_botão9.TabIndex = 8;
+            btn_botão9.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ScrollBar;
+            label1.Font = new Font("Engravers MT", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(88, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(313, 31);
+            label1.TabIndex = 9;
+            label1.Text = "Jogo da Velha";
+            // 
+            // btn_x
+            // 
+            btn_x.Location = new Point(551, 513);
+            btn_x.Name = "btn_x";
+            btn_x.Size = new Size(75, 35);
+            btn_x.TabIndex = 10;
+            btn_x.Text = "X";
+            btn_x.UseVisualStyleBackColor = true;
+            // 
+            // btn_bolinha
+            // 
+            btn_bolinha.Location = new Point(632, 513);
+            btn_bolinha.Name = "btn_bolinha";
+            btn_bolinha.Size = new Size(75, 35);
+            btn_bolinha.TabIndex = 11;
+            btn_bolinha.Text = "0";
+            btn_bolinha.UseVisualStyleBackColor = true;
+            // 
+            // btn_iniciar
+            // 
+            btn_iniciar.Location = new Point(212, 559);
+            btn_iniciar.Name = "btn_iniciar";
+            btn_iniciar.Size = new Size(75, 33);
+            btn_iniciar.TabIndex = 12;
+            btn_iniciar.Text = "INICIAR";
+            btn_iniciar.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.Location = new Point(0, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 23);
+            label2.TabIndex = 18;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(424, 466);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 15);
+            label3.TabIndex = 16;
+            // 
+            // btn_inicio
+            // 
+            btn_inicio.Location = new Point(78, 129);
+            btn_inicio.Name = "btn_inicio";
+            btn_inicio.Size = new Size(336, 409);
+            btn_inicio.TabIndex = 17;
+            btn_inicio.Text = "CLICK PARA INICIAR";
+            btn_inicio.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(510, 466);
+            label4.Name = "label4";
+            label4.Size = new Size(216, 15);
+            label4.TabIndex = 19;
+            label4.Text = "Click na opção que deseja iniciar o jogo";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(0, 64, 0);
+            ClientSize = new Size(824, 618);
+            Controls.Add(label4);
+            Controls.Add(btn_inicio);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(btn_iniciar);
+            Controls.Add(btn_bolinha);
+            Controls.Add(btn_x);
+            Controls.Add(label1);
+            Controls.Add(btn_botão9);
+            Controls.Add(btn_botão8);
+            Controls.Add(btn_botão7);
+            Controls.Add(btn_botão6);
+            Controls.Add(btn_botão5);
+            Controls.Add(btn_botão4);
+            Controls.Add(btn_botão3);
+            Controls.Add(btn_botão2);
+            Controls.Add(btn_botão1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btn_botão1;
+        private Button btn_botão2;
+        private Button btn_botão3;
+        private Button btn_botão4;
+        private Button btn_botão5;
+        private Button btn_botão6;
+        private Button btn_botão7;
+        private Button btn_botão8;
+        private Button btn_botão9;
+        private Label label1;
+        private Button btn_x;
+        private Button btn_bolinha;
+        private Button btn_iniciar;
+        private Label label2;
+        private Label label3;
+        private Button btn_inicio;
+        private Label label4;
+    }
+}
